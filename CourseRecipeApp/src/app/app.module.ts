f24d2c556c8ecc60d26cf6e106afddb6c00ee43d
Merel Ohler <merel.ohler@schedulicity.com>
2023-04-14 11:30:02 -0600

    created components

--- CourseRecipeApp/src/app/app.module.ts
+++ CourseRecipeApp/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './RecipeBook/recipe-book.component'
+import { RecipeListComponent } from './RecipeBook/recipe-item/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
