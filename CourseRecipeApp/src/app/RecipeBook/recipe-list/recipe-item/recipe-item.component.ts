5f4678315b587573e57f1732849d20d25e4ea7f5
Merel Ohler <merel.ohler@schedulicity.com>
2023-05-19 10:22:20 -0600

    recipe list updates

--- CourseRecipeApp/src/app/RecipeBook/recipe-list/recipe-item/recipe-item.component.ts
+++ CourseRecipeApp/src/app/RecipeBook/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'recipe-item',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
+
 
 }
--- CourseRecipeApp/src/app/RecipeBook/recipe-list/recipe-list.component.ts
+++ CourseRecipeApp/src/app/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.papertraildesign.com/wp-content/uploads/2017/10/Recipe-Card-3x5.jpg'),
+    new Recipe('A Second test recipe', 'This is is such a yummy recipe you HAVE to try it!!!!!', 'https://eatwithclarity.com/wp-content/uploads/2022/12/dairy-free-cake-9.jpg')
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
