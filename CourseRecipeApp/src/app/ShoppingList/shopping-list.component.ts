c564aa9ec50145d3930977da76b82134a14e606d
Merel Ohler <merel.ohler@schedulicity.com>
2023-06-16 09:37:01 -0600

    add ingredient and shopping list set up

--- CourseRecipeApp/src/app/ShoppingList/shopping-list.component.ts
+++ CourseRecipeApp/src/app/ShoppingList/shopping-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5, ''),
+    new Ingredient('sugar', 150, 'grams'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
